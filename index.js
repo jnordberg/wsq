95dcb49ddc30809e9670e099ec5eec51cfc51ff9
Johan Nordberg <its@johan-nordberg.com>
2015-11-29 19:02:08 +0100

    Typofix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	Server: require('./lib/server').Server
-	Client: require('./lib/client').Client
+	Server: require('./lib/server').Server,
+	Client: require('./lib/client').Client,
 }
